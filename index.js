aafe2a3bd46104427e235391e3fba96f567e5738
Stanislav <meetmonday@yandex.ru>
2022-01-19 01:03:40 +0500

    почистил зависимости, больше никакого красного ебала в консоли

--- index.js
+++ index.js
@@ -4,13 +4,6 @@ const axios = require('axios').default;
 
 const b = require('./bot');
 
-if (process.env.REDEBALO === true) {
-  (async () => {
-    const terminalImage = (await import('terminal-image')).default;
-    console.log(await terminalImage.file('misc/red.jpg'));
-  })();
-}
-
 if (process.argv[2] === '-wh') {
   axios.get(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/setWebhook?url=${process.env.WH_URL}`).then((e) => {
     console.log(e.data);
