8bdec1f19ac972df57fa3ccdc092453f0c0ee0ab
Stanislav <meetmonday@yandex.ru>
2022-01-19 01:40:30 +0500

    бля тайпскрипта не будет

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function hell(o, w) {
+    console.log(o);
+    return w;
+}
+console.log(hell('Hello', 'world'));
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function hell(o: string, w: string): string {
+  console.log(o);
+  return w;
+}
+
+console.log(hell('Hello', 'world'));
