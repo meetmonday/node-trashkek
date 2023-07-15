02cfa0d42fa7ba4e029d14ef43792a8ada19b8c9
Stanislav <48485055+meetmonday@users.noreply.github.com>
2023-07-15 13:31:37 +0500

    gde?
    где мне это тестить? на хоноре 16 года? хуй знает работает ли timeAgo в обратную сторону

--- src/modules/gdestas.js
+++ src/modules/gdestas.js
@@ -1,7 +1,27 @@
 import { sendMessage } from '#lib/tgApi';
 
+
+function timeAgo(ts) {
+  let fin = null;
+  const times = [
+    ['сек.', 1],
+    ['мин.', 60],
+    ['ч.', 3600],
+    ['дн.', 86400],
+  ];
+  const diff = Math.floor(Date.now() / 1000 - ts);
+  times.forEach((el) => {
+    if (diff / el[1] > 1) fin = `${Math.floor(diff / el[1])} ${el[0]}`;
+  });
+
+  return fin;
+}
+
+
+
 function main(msg) {
-  sendMessage('v armii ebat', msg);
+  const skolko = timeAgo(1713812400)
+  sendMessage(`v armii eshe ${skolko} dn. ebat`, msg);
 }
 
 export default main;
