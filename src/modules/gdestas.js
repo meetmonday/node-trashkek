1dc271a39819eb6f0204a696bfa1a118661af15f
Stanislav <48485055+meetmonday@users.noreply.github.com>
2023-07-30 14:10:19 +0500

    update src/modules/gdestas.js

--- src/modules/gdestas.js
+++ src/modules/gdestas.js
@@ -5,7 +5,7 @@ function timeAgo() {
 	const date2 = new Date("04/23/2024")
 	
 	var Difference_In_Time = date2.getTime() - date1.getTime()
-    var fin = Difference_In_Time / (1000 * 3600 * 24);
+	var fin = Difference_In_Time / (1000 * 3600 * 24);
 
 	return Math.round(fin)
 }
