c335548bc5a71b59c1cd192677a73644976fc92d
Stanislav <48485055+meetmonday@users.noreply.github.com>
2024-02-11 12:28:49 +0700

    update src/modules/hentai.js

--- src/modules/hentai.js
+++ src/modules/hentai.js
@@ -9,17 +9,37 @@ const config = process.env.HENTAI_PROXY ? {
   },
 } : {};
 
-const random = (msg) => sendMessage(link('Пикча', `https://danbooru.donmai.us/posts/${rand(0, 5013920)}`), msg);
+const random = (msg) => {
+  const randomId = rand(0, 5013920);
+  const randomImageUrl = `https://danbooru.donmai.us/posts/${randomId}`;
+  sendMessage(link('Пикча', randomImageUrl), msg);
+};
 
 function search(tags, msg) {
-  axios.get(`https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&limit=1&tags=sort:random ${tags} -animated`, config).then(({ data }) => {
-    if (data.post) sendPhoto(msg, { photo: data.post[0].file_url, caption: `score: ${data.post[0].score} / id: ${link(data.post[0].id, `https://gelbooru.com/index.php?page=post&s=view&id=${data.post[0].id}`)}` });
-    else sendMessage('Ничего не нашлось', msg);
-  }).catch((e) => {
-    sendMessage(`Ошибка: ${e.code}`, msg);
-  });
+  const searchUrl = `https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&limit=1&tags=sort:random ${tags} -animated`;
+
+  axios.get(searchUrl, config)
+    .then(({ data }) => {
+      if (data.post) {
+        const post = data.post[0];
+        const photo = post.file_url;
+        const caption = `score: ${post.score} / id: ${link(post.id, \`https://gelbooru.com/index.php?page=post&s=view&id=${post.id}`)}`;
+        sendPhoto(msg, { photo, caption });
+      } else {
+        sendMessage('Ничего не нашлось', msg);
+      }
+    })
+    .catch((e) => {
+      sendMessage(`Ошибка: ${e.code}`, msg);
+    });
 }
 
-const hentaiRouter = (msg, args) => (args ? search(args, msg) : random(msg));
+const hentaiRouter = (msg, args) => {
+  if (args) {
+    search(args, msg);
+  } else {
+    random(msg);
+  }
+};
 
 export default hentaiRouter;
