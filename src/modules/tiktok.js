a5564153c9bb8f7206008c506544b9d078a6d075
Stanislav <48485055+meetmonday@users.noreply.github.com>
2024-03-14 21:24:31 +0800

    update src/modules/tiktok.js

--- src/modules/tiktok.js
+++ src/modules/tiktok.js
@@ -1,9 +1,9 @@
-import { legacyDownload } from '#lib/tswwMini';
+import { ttd } from '#lib/tswwMini';
 import { deleteMessage, sendMessage, sendVideo } from '#lib/tgApi';
 
 function main(msg) {
   sendMessage('Ща будет текток.........', msg, {}, ({ result: del }) => {
-    legacyDownload(msg.text)
+    ttd(msg.text)
       .then((ttres) => {
         sendVideo(msg, { video: ttres });
         deleteMessage({ chat: { id: del.chat.id }, message_id: del.message_id });
