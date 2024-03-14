a97b9654996bf0c285a9fa384358a082419665cd
Stanislav <48485055+meetmonday@users.noreply.github.com>
2024-03-14 21:22:22 +0800

    update src/lib/tswwMini.js

--- src/lib/tswwMini.js
+++ src/lib/tswwMini.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+import cheerio from 'cheerio'
 const INSTANCE = 'https://proxitok.pabloferreiro.es/';
 function legacyDownload(url) {
   const path = url.match(/tiktok.com\/(.*)\//)[1];
@@ -25,4 +25,45 @@ function tiktokdownload(url) {
   });
 }
 
-export { legacyDownload, tiktokdownload };
+function ttd(url) {
+     return new Promise((resolve, reject) => {
+          Axios.get('https://ttdownloader.com/')
+               .then((data) => {
+                    const $ = cheerio.load(data.data)
+                    const cookie = data.headers['set-cookie'].join('')
+                    const dataPost = {
+                         url: url,
+                         format: '',
+                         token: $('#token').attr('value')
+                    }
+                    // return console.log(cookie);
+                    Axios({
+                         method: 'POST',
+                         url: 'https://ttdownloader.com/search/',
+                         headers: {
+                              'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                              origin: 'https://ttdownloader.com',
+                              referer: 'https://ttdownloader.com/',
+                              cookie
+                         },
+                         data: qs.stringify(dataPost)
+                    }).then(({ data }) => {
+                         const $ = cheerio.load(data)
+                         const result = {
+                              nowm: $('#results-list > div:nth-child(2) > div.download > a')?.attr('href'),
+                              wm: $('#results-list > div:nth-child(3) > div.download > a')?.attr('href'),
+                              audio: $('#results-list > div:nth-child(4) > div.download > a').attr('href')
+                         }
+                         resolve(result);
+                    })
+                         .catch(e => {
+                              reject({ status: false, message: 'error fetch data', e: e.message })
+                         })
+               })
+               .catch(e => {
+                    reject({ status: false, message: 'error fetch data', e: e.message })
+               })
+     })
+}
+
+export { legacyDownload, tiktokdownload, ttd };
