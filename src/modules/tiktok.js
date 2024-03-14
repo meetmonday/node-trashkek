e5e58d4dce32360405ada2e55465ab63c45a3093
Egor Taranov <dev@taranovegor.com>
2024-03-14 10:58:35 +0100

    Revert "update src/modules/tiktok.js"
    This reverts commit 88b6818849339e4dab153670271abc10995a0645.

--- src/modules/tiktok.js
+++ src/modules/tiktok.js
@@ -1,9 +1,9 @@
-import { tiktokDownload } from '#lib/tswwMini';
+import { legacyDownload } from '#lib/tswwMini';
 import { deleteMessage, sendMessage, sendVideo } from '#lib/tgApi';
 
 function main(msg) {
   sendMessage('Ща будет текток.........', msg, {}, ({ result: del }) => {
-    tiktokDownload(msg.text)
+    legacyDownload(msg.text)
       .then((ttres) => {
         sendVideo(msg, { video: ttres });
         deleteMessage({ chat: { id: del.chat.id }, message_id: del.message_id });
