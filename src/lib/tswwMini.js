80b5d19195335f371c3e087b68efa1a2fd5615a6
Egor Taranov <dev@taranovegor.com>
2024-03-19 14:29:28 +0100

    тикток довнлоатер апдэйтыд

--- src/lib/tswwMini.js
+++
@@ -1,8 +0,0 @@
-import pkg from 'nayan-media-downloader';
-const { tikdown } = pkg;
-
-async function ttd(url) {
-    return await tikdown(url)
-}
-
-export { ttd };
--- src/modules/tiktok.js
+++ src/modules/tiktok.js
@@ -1,15 +1,16 @@
-import { ttd } from '#lib/tswwMini';
 import { deleteMessage, sendMessage, sendVideo } from '#lib/tgApi';
+import pkg from 'nayan-media-downloader';
+const { tikdown } = pkg;
 
 function main(msg) {
-  sendMessage('Ща будет текток.........', msg, {}, ({ result: del }) => {
-    ttd(msg.text)
-      .then((ttres) => {
-        sendVideo(msg, { video: ttres.data.video });
-        deleteMessage({ chat: { id: del.chat.id }, message_id: del.message_id });
-      })
-      .catch(() => sendMessage('Чета не так пошло........', msg));
-  });
+    sendMessage('Ща будет текток.........', msg, {}, ({ result: del }) => {
+        tikdown(msg.text)
+            .then((ttres) => {
+                sendVideo(msg, { video: ttres.data.video });
+                deleteMessage({ chat: { id: del.chat.id }, message_id: del.message_id });
+            })
+            .catch(() => sendMessage('Чета не так пошло........', msg));
+    });
 }
 
 export default main;
