5875b466d73f91a69e3f9f3a4105f720b1778458
Egor Taranov <dev@taranovegor.com>
2024-04-23 15:52:10 +0200

    stas uze vernuls'ya

--- src/modules/gdestas.js
+++ src/modules/gdestas.js
@@ -1,18 +1,7 @@
 import { sendMessage } from '#lib/tgApi';
 
-function timeAgo() {
-	const date1 = new Date()
-	const date2 = new Date("04/23/2024")
-	
-	var Difference_In_Time = date2.getTime() - date1.getTime()
-	var fin = Difference_In_Time / (1000 * 3600 * 24);
-
-	return Math.round(fin)
-}
-
 function main(msg) {
-  const skolko = timeAgo()
-  sendMessage(`v armii eshe ${skolko} dn. ebat`, msg);
+  sendMessage(`doma`, msg);
 }
 
 export default main;
