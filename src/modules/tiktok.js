bdf5ed212579f5f7aac65b1a4f8e722f05007f92
Stanislav <48485055+meetmonday@users.noreply.github.com>
2024-06-11 12:21:58 +0000

    новый качатель тиктоков (С КАРТИНКАМИ)

--- src/modules/tiktok.js
+++ src/modules/tiktok.js
@@ -1,17 +1,46 @@
-import pkg from 'nayan-media-downloader';
-const { tikdown } = pkg;
+import axios from "axios";
+import tl from '#lib/temploader';
+import fs from 'fs'
 
 function main(ctx) {
-  ctx.persistentChatAction('upload_video', () => {
-    tikdown(ctx.update.message.text)
-      .then((ttres) => {
-        ctx.sendVideo(ttres.data.video, {
-          caption: ttres.data.title
-        });
-      })
-      .catch(() => ctx.sendMessage('Чета не так пошло........'));
-  })
+  ctx.persistentChatAction("upload_video", () => {
+    axios
+      .post(
+        "https://api.cobalt.tools/api/json",
+        {
+          url: ctx.update.message.text,
+        },
+        {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then(({ data }) => {
+        console.log(data)
+        if(data.status === 'picker') {
+          const photos = data.picker.slice(0,9).map((e) => {
+            return {
+              type: 'photo',
+              media: e.url
+            }
+          })
 
+          ctx.sendMediaGroup(photos)
+          tl.downloadFile(data.audio).then(e=> {
+            console.log(e)
+            ctx.sendVoice({ source : fs.readFileSync(e) })
+            tl.deleteFile(e)
+          })
+        } else {
+          tl.downloadFile(data.url).then(e=> {
+            ctx.sendVideo({ source : fs.readFileSync(e) })
+            tl.deleteFile(e)
+          })
+        }
+      });
+  });
 }
 
 export default main;
